717450368e716ae96ca5982b10f98a294baa180d
JP Smith <jp@writes.co.de>
2019-06-20 12:31:24 -0400

    make things more portable by vendoring + shell scripts

--- script/trace_unsafety.py
+++ script/trace_unsafety.py
@@ -3,6 +3,13 @@ import re
 import sys
 import logging
 import os
+import sys
+
+# Add vendor directory to module search path
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+
+sys.path.append(vendor_dir)
 
 import networkx as nx
 
