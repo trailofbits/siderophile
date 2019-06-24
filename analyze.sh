b958433efaa7bc61d9e3f24917d179d9c8905088
Michael Rosenberg <micro@fastmail.com>
2019-06-24 13:15:13 -0400

    Cleaned up analyze.sh a bit

--- analyze.sh
+++ analyze.sh
@@ -4,40 +4,54 @@
 # -e ensures that the script dies if a command fails with a nonzero error code
 set -ue
 
+function usage() {
+    echo "USAGE: $(basename "$0") [bin|lib] CRATENAME"
+}
+
+function reqs() {
+    echo "See siderophile's README for detailed requirements"
+}
+
+if !(hash "cargo") 2>/dev/null; then
+    echo "siderophile requires cargo, which doesn't seem to be installed"
+    reqs
+    exit 1
+fi
+
+if !(hash "python3") 2>/dev/null; then
+    echo "siderophile requires Python 3, which doesn't seem to be installed"
+    reqs
+    exit 1
+fi
+
+if !(hash "opt") 2>/dev/null; then
+    echo "siderophile requires LLVM utilities, which don't seem to be installed"
+    reqs
+    exit 1
+fi
+
+# This script takes precisely 1 argument
+if [ "$#" -ne 1 ]; then
+    usage
+    exit 1
+fi
+
+# The name of the crate we're analyzing
+CRATENAME=$1
+
 # The folder that this bash file is in
 SIDEROPHILE_PATH=$(dirname "$0")
 
 # The folder we output all the files into
 SIDEROPHILE_OUT="siderophile_out"
 
-if !(hash "cargo") 2>/dev/null; then
-    echo "siderophile requires cargo, which doesn't seem to be installed"
-    echo "See siderophile's README for detailed requirements"
-    exit 1
-fi
-
-if !(hash "python3") 2>/dev/null; then
-    echo "siderophile requires Python 3, which doesn't seem to be installed"
-    echo "See siderophile's README for detailed requirements"
-    exit 1
-fi
-
-if !(hash "opt") 2>/dev/null; then
-    echo "siderophile requires LLVM utilities, which don't seem to be installed"
-    echo "See siderophile's README for detailed requirements"
-    exit 1
-fi
-
-if [ -z "$1" ]; then
-    echo "USAGE: ./$(basename $0) CRATENAME"
-    exit 1
-fi
 
 if !(test -x "$SIDEROPHILE_PATH/target/release/siderophile") 2>/dev/null; then
     echo "couldn't find siderophile binary, did you run setup.sh?"
     exit 1
 fi
 
+# All auxiliary files go here
 mkdir -p $SIDEROPHILE_OUT
 
 echo "trawling source code of dependencies for unsafety"
@@ -50,7 +64,7 @@ CARGO_INCREMENTAL="0" \
 cargo rustc --lib -- --emit=llvm-bc
 
 echo "generating callgraph"
-opt -dot-callgraph ./target/debug/deps/$1-*.bc
+opt -dot-callgraph ./target/debug/deps/$CRATENAME-*.bc
 # This outputs to ./callgraph.dot no matter what. Move it
 mv ./callgraph.dot "$SIDEROPHILE_OUT/mangled_callgraph.dot"
 
@@ -73,7 +87,7 @@ echo "tracing the unsafety up the tree"
 python3 "$SIDEROPHILE_PATH/script/trace_unsafety.py" \
     "$SIDEROPHILE_OUT/unmangled_callgraph.dot" \
     "$SIDEROPHILE_OUT/nodes_to_taint.txt" \
-    "$1" \
+    "$CRATENAME" \
     > "$SIDEROPHILE_OUT/badness.txt"
 
 echo "done. see $SIDEROPHILE_OUT/badness.txt"
