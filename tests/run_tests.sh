941f058935fd563f7d4c5a020076155d34f9ff11
disconnect3d <dominik.b.czarnota@gmail.com>
2020-01-06 20:48:30 +0100

    Add test against crates with rust-toolchain
    This test crashes as of now and will be fixed in the next commit:
    ```
    trawling source code of dependencies for unsafety
        Checking bitvec v0.15.2
        Checking crate-uses-rust-toolchain v0.1.0 (/Users/dc/tob/projects/siderophile/tests/crate-use-rust-toolchain)
      1 // This program tests if we can use Siderophile with crates which dependencies
    error[E0463]: can't find crate for `bitvec`                              ] 1/2: crate-uses-rust-toolchain
     --> src/lib.rs:9:1
      |
    9 | extern crate bitvec;
      | ^^^^^^^^^^^^^^^^^^^^ can't find crate
    
    error: aborting due to previous error
    
    For more information about this error, try `rustc --explain E0463`.
    thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: Cargo("Could not compile `crate-uses-rust-toolchain`.")', src/libcore/result.rs:1187:5
    ```

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-set -e
+TESTS=(inlining librarycrate crate-use-rust-toolchain)
 
-TESTS=(inlining librarycrate)
+# https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-printf-in-linux
+INFO='\033[1;33m'   # Yellow
+OK='\033[0;32m'     # Green
+WARN='\033[0;31m'   # Red
+NC='\033[0m'        # No Color
 
-echo "[!!!] Tests that will be run (space-delimited): ${TESTS[*]}"
+printf "$INFO[!!!] Tests that will be run (space-delimited): ${TESTS[*]}$NC\n"
 echo ""
 
-for testdir in $TESTS; do
-    echo "[@@@] Going to run '$testdir' test"
+for testdir in ${TESTS[@]}; do
+    printf "$INFO[@@@] Going to run '$testdir' test$NC\n"
     echo ""
     pushd $testdir
     ../../analyze.sh $testdir
     if ! (diff ./expected_badness.txt ./siderophile_out/badness.txt); then
         echo ""
-        echo "[!!!] Tests failed on $testdir: the expected_badness.txt does not match the siderophile_out/badness.txt file!"
+        printf "$WARN[!!!] Tests failed on $testdir: the expected_badness.txt does not match the siderophile_out/badness.txt file!$NC\n"
         exit 1
     fi
     popd
 done
 
 echo ""
-echo "[+++] Tests succeeded!"
+printf "$OK[+++] Tests succeeded!$NC\n"
