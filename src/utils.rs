276d235c166258a6975b3a128c383566be88a332
Claudia Richoux <claudia.richoux@trailofbits.com>
2020-07-18 22:14:09 -0400

    learning new things every day- this time, it's that cargo check doesn't check tests

--- src/utils.rs
+++ src/utils.rs
@@ -35,29 +35,29 @@ pub fn simplify_trait_paths(path: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::callgraph_matching::simplify_trait_paths;
+    use crate::utils::simplify_trait_paths;
 
     #[test]
     fn test_1() {
-        assert_eq!(simplify_trait_paths("<&mut std::collections::hash::table::RawTable<K,V> as std::collections::hash::table::Put<K,V>>::borrow_table_mut".to_string()), "<&mut std::collections::hash::table::RawTable<K,V> as Put<K,V>>::borrow_table_mut");
+        assert_eq!(simplify_trait_paths("<&mut std::collections::hash::table::RawTable<K,V> as std::collections::hash::table::Put<K,V>>::borrow_table_mut"), "<&mut std::collections::hash::table::RawTable<K,V> as Put<K,V>>::borrow_table_mut");
     }
     #[test]
     fn test_2() {
         assert_eq!(
             simplify_trait_paths(
-                "<futures::lock::TryLock<T> as core::ops::deref::Deref>::deref".to_string()
+                "<futures::lock::TryLock<T> as core::ops::deref::Deref>::deref"
             ),
             "<futures::lock::TryLock<T> as Deref>::deref"
         );
     }
     #[test]
     fn test_3() {
-        assert_eq!(simplify_trait_paths("<network::proto::state_synchronizer::RequestChunk as ::protobuf::Message>::default_instance".to_string()), "<network::proto::state_synchronizer::RequestChunk as Message>::default_instance");
+        assert_eq!(simplify_trait_paths("<network::proto::state_synchronizer::RequestChunk as ::protobuf::Message>::default_instance"), "<network::proto::state_synchronizer::RequestChunk as Message>::default_instance");
     }
     #[test]
     fn test_4() {
         assert_eq!(
-            simplify_trait_paths("<T as failure::as_fail::AsFail>::as_fail".to_string()),
+            simplify_trait_paths("<T as failure::as_fail::AsFail>::as_fail"),
             "<T as AsFail>::as_fail"
         );
     }
